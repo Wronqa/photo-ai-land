adf449abc9eb80a9c2c45aae821a1157cb7e4f6f
Wronqa <kuba.wrona@onet.pl>
2023-11-07 22:39:24 +0100

    Created form modal

---
+++ src/app/modules/profile/components/add-post/add-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-post',
+  templateUrl: './add-post.component.html',
+  styleUrls: ['./add-post.component.scss'],
+})
+export class AddPostComponent {
+  text!: string;
+  uploadedFiles: any[] = [];
+}
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -3,11 +3,26 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { SharedModule } from '../shared/shared.module';
 import { ButtonModule } from 'primeng/button';
-
+import { EditorModule } from 'primeng/editor';
 import { DialogModule } from 'primeng/dialog';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { FileUploadModule } from 'primeng/fileupload';
+import { TagModule } from 'primeng/tag';
 
 @NgModule({
-  declarations: [ProfileComponent],
-  imports: [CommonModule, SharedModule, ButtonModule, DialogModule],
+  declarations: [ProfileComponent, AddPostComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ButtonModule,
+    DialogModule,
+    EditorModule,
+    FormsModule,
+    InputTextModule,
+    FileUploadModule,
+    TagModule,
+  ],
 })
 export class ProfileModule {}
