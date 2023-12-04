d247eb373413411fa7d25c961eb6ad65110750fb
Wronqa <kuba.wrona@onet.pl>
2023-12-04 22:13:30 +0100

    Fixes

--- src/app/modules/shared/components/followers-list/followers-list.component.ts
+++ src/app/modules/shared/components/followers-list/followers-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { concatMap, forkJoin } from 'rxjs';
+import { concatMap, defaultIfEmpty, forkJoin, tap } from 'rxjs';
 import { selectUser } from 'src/app/modules/store/user/user.selectors';
 import { IUser } from '../../interfaces/user.interface';
 import { UserService } from 'src/app/modules/core/services/user.service';
@@ -12,21 +12,29 @@ import { UserService } from 'src/app/modules/core/services/user.service';
 })
 export class FollowersListComponent {
   protected followings: IUser[] = [];
+  isLoading = false;
   constructor(private store: Store, private userService: UserService) {}
 
   ngOnInit(): void {
     this.store
       .pipe(select(selectUser))
       .pipe(
+        tap(() => (this.isLoading = true)),
         concatMap((user: IUser) => {
           return forkJoin(
             user.followers.map((username) => this.userService.getUser(username))
-          );
+          ).pipe(defaultIfEmpty([]));
         })
       )
-      .subscribe((users) => {
-        console.log(users);
-        this.followings = users;
+      .subscribe({
+        next: (users) => {
+          console.log(users);
+          this.followings = users;
+          this.isLoading = false;
+        },
+        error: () => {
+          this.isLoading = false;
+        },
       });
   }
 }
